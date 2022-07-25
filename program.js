dc3c2999cd62c301fd1333b28fe7e68aff86ebef
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 11:22:04 +0530

    Add files via upload

--- program.js
+++ program.js
@@ -1,2 +1,11 @@
-//Output the string "Hello ES6" to the console
-console.log("HELLO ES6");
+/*
+Replace the function in the original code with an arrow function,
+since the this value inside of an arrow function is the same as this value outside of it.
+*/
+var foot = {
+    kick: function () {
+        this.yelp = "Ouch!";
+        setImmediate(() => console.log(this.yelp));
+    }
+};
+foot.kick();
