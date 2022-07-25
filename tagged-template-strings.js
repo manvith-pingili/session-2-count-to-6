5a02b8d902ab2f29f6da2b603d95b764ebcea625
manvith-pingili <109732855+manvith-pingili@users.noreply.github.com>
2022-07-25 13:23:25 +0530

    Update tagged-template-strings.js

--- tagged-template-strings.js
+++ tagged-template-strings.js
@@ -1,4 +1,6 @@
+//tagging the template string using the html function 
 console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+//the function html takes two arguments in pieces and substitutions
 function html(pieces, ...substitutions) {
     let result = pieces[0];
     for(let i=0; i<substitutions.length; i++) {
@@ -6,6 +8,7 @@ function html(pieces, ...substitutions) {
     }
     return result;
 }
+//the safe function returns the substitutions by replacing HTML-unsafe characters with safely-escaped HTML rendering of the comment
 function safe(str) {
     return str.replace(/&/g, "&amp;")
             .replace(/</g, "&lt;")
